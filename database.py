f3e7dc088b6fec2201c667b6297d5d8c89075427
zyphi <donzyphius@gmail.com>
2023-04-25 17:33:30 +0200

    20230425

---
+++ database.py
@@ -0,0 +1,83 @@
+import sqlite3
+import os
+
+from colors import Col
+
+
+class Database:
+    def __init__(self):
+        self.__create_db_folder()
+        self.conn = sqlite3.connect('db/ffwrap.db')
+        self.cur = self.conn.cursor()
+        self.__create_table()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close_connection()
+
+    def __create_db_folder(self):
+        if not os.path.isdir('db'):
+            os.mkdir('db')
+
+    def __create_table(self):
+        self.cur.execute(
+            f"CREATE TABLE IF NOT EXISTS renders (id INTEGER PRIMARY KEY, command TEXT, inputs TEXT, outputs TEXT, total_time INTEGER, exit_code INTEGER, duplicated_frames INTEGER, dropped_frames INTEGER, start_time DATETIME, end_time DATETIME)")
+        self.conn.commit()
+
+    def __format_entries(self, entries: list[tuple]) -> str:
+        formatted_entries = []
+        for e in entries:
+            formatted_entries.append('\n'.join([
+                f'ID: {e[0]}',
+                f'  FFmpeg command: "{Col.blue}{e[1]}{Col.endc}"',
+                f'  Inputs: {e[2].split(":::")}',
+                f'  Outputs: {e[3].split(":::")}',
+                f'  Total time: {Col.green if e[4] else Col.fail}{e[4]}{"s" if e[4] else ""}{Col.endc} (start: {e[8]}, end: {e[9]})',
+                f'  Exit code: {Col.blue if e[5] == 0 else Col.fail}{e[5]}{Col.endc}' +
+                (f' (dup: {e[6]}, drop: {e[7]})' if e[6] or e[7] else '')
+            ]))
+        return '\n\n'.join(formatted_entries)
+
+    def close_connection(self):
+        self.conn.close()
+
+    def init_before_render(
+            self,
+            command: str,
+            inputs: str,
+            outputs: str,
+            start_time: str,
+    ) -> int | None:
+        self.cur.execute(
+            f"INSERT INTO renders (command, inputs, outputs, start_time) VALUES (?, ?, ?, ?)",
+            (command, inputs, outputs, start_time)
+        )
+        self.conn.commit()
+        return self.cur.lastrowid
+
+    def update_after_render(self, id: int, total_time: int, end_time: str, exit_code: int, duplicated_frames: int, dropped_frames: int):
+        self.cur.execute(
+            f"UPDATE renders SET total_time=?, end_time=?, exit_code=?, duplicated_frames=?, dropped_frames=? WHERE id=?",
+            (total_time, end_time, exit_code, duplicated_frames, dropped_frames, id)
+        )
+        self.conn.commit()
+
+    def get_all(self):
+        self.cur.execute("SELECT * FROM renders")
+        return self.__format_entries(self.cur.fetchall())
+
+    def find_by_input(self, i: str):
+        self.cur.execute(
+            "SELECT * FROM renders WHERE inputs LIKE ?", (f'%{i}%',))
+        return self.__format_entries(self.cur.fetchall())
+
+    def find_by_output(self, o: str):
+        self.cur.execute(
+            "SELECT * FROM renders WHERE outputs LIKE ?", (f'%{o}%',))
+        return self.__format_entries(self.cur.fetchall())
+
+
+with Database() as db:
+    print(db.get_all())
--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from datetime import datetime
 from io_handler import parse_args, print_command, print_metadata, check_inputs, check_outputs, print_result
 from misc import format_timedelta
 from subprocess_handler import spawn_ffmpeg
+from database import Database
 
 
 if __name__ == '__main__':
     start_time = datetime.now()
     command, inputs, outputs = parse_args(sys.argv[1:])
+    db_id = None
+    with Database() as db:
+        db_id = db.init_before_render(
+            command=' '.join(command),
+            inputs=':::'.join(inputs),
+            outputs=':::'.join(outputs),
+            start_time=start_time.strftime('%Y-%m-%d %H:%M:%S')
+        )
     print_command(command)
 
     check_inputs(inputs)
@@ -18,5 +27,17 @@ if __name__ == '__main__':
 
     exit_code, duplicated_frames, dropped_frames = spawn_ffmpeg(command)
 
+    end_time = datetime.now()
     print_result(exit_code, duplicated_frames,
-                 dropped_frames, format_timedelta(datetime.now() - start_time))
+                 dropped_frames, format_timedelta(end_time - start_time))
+
+    if db_id:
+        with Database() as db:
+            db.update_after_render(
+                id=db_id,
+                total_time=int((end_time - start_time).total_seconds()),
+                end_time=end_time.strftime('%Y-%m-%d %H:%M:%S'),
+                exit_code=exit_code,
+                duplicated_frames=duplicated_frames,
+                dropped_frames=dropped_frames
+            )
