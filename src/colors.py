7dae22434a0eb5069a7db49563de85f4e296d491
zyphi <donzyphius@gmail.com>
2023-04-26 10:21:57 +0200

    20230426

--- src/colors.py
+++ src/colors.py
@@ -1,12 +1,17 @@
+import platform
+
+is_win = platform.system() == 'Windows'
+
+
 class Col:
-    default = '\033[39m'
-    header = '\033[95m'
-    blue = '\033[94m'
-    cyan = '\033[96m'
-    green = '\033[92m'
-    yellow = '\033[93m'
-    warn = '\033[35m'
-    fail = '\033[91m'
-    bold = '\033[1m'
-    underline = '\033[4m'
-    endc = '\033[0m'
+    default = '\033[39m' if not is_win else ''
+    header = '\033[95m' if not is_win else ''
+    blue = '\033[94m' if not is_win else ''
+    cyan = '\033[96m' if not is_win else ''
+    green = '\033[92m' if not is_win else ''
+    yellow = '\033[93m' if not is_win else ''
+    warn = '\033[35m' if not is_win else ''
+    fail = '\033[91m' if not is_win else ''
+    bold = '\033[1m' if not is_win else ''
+    underline = '\033[4m' if not is_win else ''
+    endc = '\033[0m' if not is_win else ''
